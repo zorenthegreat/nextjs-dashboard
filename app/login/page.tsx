270c9344e2c69c2d66ebb4c1f0ddd737641eb23d
zorenthegreat <renrenaguilargates@gmail.com>
2024-01-28 17:17:40 +0800

    added metadata

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
  import { Metadata } from 'next';
 
  export const metadata: Metadata = {
-    title: 'Login',
+    title: 'Login Page',
   };
 
 export default function LoginPage() {
