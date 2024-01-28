7dd280cb6ad84cf5b261d3399ddf6db62eab5e72
zorenthegreat <renrenaguilargates@gmail.com>
2024-01-28 17:03:09 +0800

    added some features

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <GlobeAltIcon className="h-20 w-20 rotate-[15deg]" />
+      <p className="text-[44px]">BudgetVue</p>
     </div>
   );
 }
