959cadc0658a7c4df090d5adf0254bcefae17366
zorenthegreat <renrenaguilargates@gmail.com>
2024-02-12 22:22:15 +0800

    update: UI

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,9 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+           Welcome to <strong> BudgetVue</strong> Empower Your Finances,
+            Embrace Financial Freedom – BudgetVue, Your Path to Smart Spending
+            and Saving.
           </p>
           <Link
             href="/login"
